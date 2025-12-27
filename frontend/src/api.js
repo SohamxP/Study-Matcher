0fb450c797dfab8fb03e2a8bdbb2f00d573ba713
Soham Panchal <work.panchalsoham@gmail.com>
2025-12-26 20:10:13 -0600

    Update api url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://study-matcher-backend.onrender.com';
+const API_URL = 'https://study-matcher-backend.onrender.com/api';
 
 // Create axios instance
 const api = axios.create({
