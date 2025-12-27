918732a0977b83550d34ede4d19b3facd2f08ab7
Soham Panchal <work.panchalsoham@gmail.com>
2025-12-26 19:54:28 -0600

    Update frontend to use production backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3000/api';
+const API_URL = 'https://study-matcher-backend.onrender.com';
 
 // Create axios instance
 const api = axios.create({
